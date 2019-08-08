d64c497b8a06bc502aaec5211feca59666056f97
repenno <rapenno@gmail.com>
2019-08-08 13:44:27 -0700

    Envoy Proxy with Redirection

--- cmd/redirect-outgoing/build_docker.sh
+++ cmd/redirect-outgoing/build_docker.sh
@@ -14,4 +14,4 @@ docker stop ${CONTAINER_NAME} || true
 docker rm ${CONTAINER_NAME} || true
 docker rmi -f ${CONTAINER_NAME} || true
 docker build -f ${DOCKERFILE} -t ${CONTAINER_NAME} .
-docker run -d -p "${PORT}":"${PORT}" -p 19000:19000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
+docker run -d --network host --cap-add NET_ADMIN -p "${PORT}":"${PORT}" -p 19000:19000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
--- cmd/redirect-outgoing/iptables.sh
+++ cmd/redirect-outgoing/iptables.sh
@@ -2,3 +2,27 @@
 set -e
 
 sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 4999
+
+sudo iptables -t nat -L --line-numbers
+#Chain PREROUTING (policy ACCEPT)
+#num  target     prot opt source               destination
+#1    DOCKER     all  --  anywhere             anywhere             ADDRTYPE match dst-type LOCAL
+#
+#Chain INPUT (policy ACCEPT)
+#num  target     prot opt source               destination
+#
+#Chain OUTPUT (policy ACCEPT)
+#num  target     prot opt source               destination
+#1    DOCKER     all  --  anywhere            !localhost/8          ADDRTYPE match dst-type LOCAL
+#2    REDIRECT   tcp  --  anywhere             anywhere             tcp dpt:http redir ports 4999
+#
+#Chain POSTROUTING (policy ACCEPT)
+#num  target     prot opt source               destination
+#1    MASQUERADE  all  --  ip-172-17-0-0.us-west-1.compute.internal/16  anywhere
+#
+#Chain DOCKER (2 references)
+#num  target     prot opt source               destination
+#1    RETURN     all  --  anywhere             anywhere
+
+# Deleting
+# sudo iptables -t nat -D OUTPUT 2
