dcb29cd9e0e78040f24b60562268bca899502b57
repenno <rapenno@gmail.com>
2019-08-07 23:42:54 -0700

    Small fixes on iptables shell script

--- cmd/tproxy-outgoing/iptables.sh
+++ cmd/tproxy-outgoing/iptables.sh
@@ -53,7 +53,7 @@ sudo iptables -j TPROXY --help
 # new chain DIVERT in mangle table for marking
 sudo iptables -t mangle -N DIVERT
 sudo iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT
-sudo iptables -t mangle -A DIVERT -j MARK --set-mark 7
+sudo iptables -t mangle -A DIVERT -j MARK --set-mark 1
 sudo iptables -t mangle -A DIVERT -j ACCEPT
 
 # Verify
@@ -82,14 +82,14 @@ sudo iptables -t mangle -A DIVERT -j ACCEPT
 
 
 # new rule & route for marked
-sudo ip rule add fwmark 7 lookup 100
+sudo ip rule add fwmark 0x1 lookup 100
 sudo ip route add local 0.0.0.0/0 dev lo table 100
 
 # Verify new rules & route
 
 #ubuntu:~$ ip rule list
 #0:	from all lookup local
-#32765:	from all fwmark 0x7 lookup 123
+#32765:	from all fwmark 0x1 lookup 100
 #32766:	from all lookup main
 #32767:	from all lookup default
 
@@ -120,4 +120,4 @@ sudo ip route add local 0.0.0.0/0 dev lo table 100
 #local fe80::40:ff:fe46:51d3 dev eth0 table local proto kernel metric 0 pref medium
 #ff00::/8 dev eth0 table local metric 256 pref medium
 
-sudo iptables -t mangle -A PREROUTING -p tcp --dport 80 -j TPROXY --tproxy-mark 0x7/0x7 --on-port 4999
+# sudo iptables -t mangle -A PREROUTING -p tcp --dport 80 -j TPROXY --tproxy-mark 0x1/0x1 --on-port 4999
