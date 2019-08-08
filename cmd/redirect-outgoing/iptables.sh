800f183d1e091ac065fa6b5f979bd8298b754478
repenno <rapenno@gmail.com>
2019-08-08 14:21:55 -0700

    Envoy Proxy with Redirection

--- cmd/redirect-outgoing/iptables.sh
+++ cmd/redirect-outgoing/iptables.sh
@@ -1,7 +1,7 @@
 # Enable exit on non 0
 set -e
 
-sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 4999
+sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 80 -j REDIRECT --to-port 4999
 sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8443
 
 sudo iptables -t nat -L --line-numbers
