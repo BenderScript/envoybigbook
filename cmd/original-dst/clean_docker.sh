b36a87e1d9f754a7434db553a82da0f9873a5180
repenno <rapenno@gmail.com>
2019-08-09 09:56:13 -0700

    Original Dest example

---
+++ cmd/original-dst/clean_docker.sh
@@ -0,0 +1,8 @@
+# Enable exit on non 0
+set -e
+
+CONTAINER_NAME=envoy-original-dest
+
+docker stop ${CONTAINER_NAME} || true
+docker rm ${CONTAINER_NAME} || true
+docker rmi -f ${CONTAINER_NAME} || true
