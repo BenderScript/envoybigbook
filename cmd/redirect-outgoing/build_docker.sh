0c0cc24e8cc6997817b3d79852ac2e25cf22555f
repenno <rapenno@gmail.com>
2019-08-08 19:59:36 -0700

    Fixing script for redirect example

--- cmd/redirect-outgoing/build_docker.sh
+++ cmd/redirect-outgoing/build_docker.sh
@@ -20,4 +20,4 @@ docker stop ${CONTAINER_NAME} || true
 docker rm ${CONTAINER_NAME} || true
 docker rmi -f ${CONTAINER_NAME} || true
 docker build -f ${DOCKERFILE} -t ${CONTAINER_NAME} .
-docker run -d --network host --cap-add NET_ADMIN -p "${PORT}":"${PORT}" -p "${HTTPS_PORT}":"${HTTPS_PORT}" -p 19000:19000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
+docker run -it -p "${PORT}":"${PORT}" -p "${HTTPS_PORT}":"${HTTPS_PORT}" -p 19000:19000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
--- cmd/redirect-outgoing/envoy.Dockerfile
+++ cmd/redirect-outgoing/envoy.Dockerfile
@@ -4,13 +4,10 @@ EXPOSE 4999
 EXPOSE 19000
 EXPOSE 8443
 
-RUN apt-get update
-RUN apt-get install -y iputils-ping
-RUN apt-get install -y curl
+RUN apt-get update -qq && apt-get install -qq iputils-ping
+RUN apt-get install -qq curl
 
 ADD ./service-envoy.yaml /etc/service-envoy.yaml
 ADD ./start_envoy.sh /usr/local/bin/start_envoy.sh
 RUN chmod u+x /usr/local/bin/start_envoy.sh
 ENTRYPOINT /usr/local/bin/start_envoy.sh
-
-
