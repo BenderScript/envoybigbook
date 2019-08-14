4a9de10e18fc2523866d1df5568dab75e13e2ad2
repenno <rapenno@gmail.com>
2019-08-13 20:40:18 -0700

    Refactoring names

--- cmd/simple-front-proxy/build_run_all.sh
+++ cmd/simple-front-proxy/build_run_all.sh
@@ -1,10 +1,14 @@
 # Enable exit on non 0
 set -e
 
-printf "%s\n" "Building and running Envoy Docker"
+printf "%s\n\n" "Building and running Envoy Docker"
+printf "%s"     "================================="
 
 . build_envoy_docker.sh
 
 printf "%s\n" "Starting Web Server"
+printf "%s"   "==================="
 
 . run_web_docker.sh
+
+printf "\n\n"
