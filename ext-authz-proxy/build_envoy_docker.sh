2adb96e988409feb707c865d0399e2d220c27502
repenno <rapenno@gmail.com>
2019-08-19 11:42:26 -0700

    Improving README

--- ext-authz-proxy/build_envoy_docker.sh
+++ ext-authz-proxy/build_envoy_docker.sh
@@ -1,17 +1,22 @@
 # Enable exit on non 0
 set -e
 
+. clean_envoy_docker.sh
+
 if [ -z "${ENVOY_PORT}" ]; then
   PORT=4999
 else
   PORT="${ENVOY_PORT}"
 fi
 
+if [ -z "${ENVOY_ADMIN_PORT}" ]; then
+  ADMIN_PORT=19000
+else
+  ADMIN_PORT="${ENVOY_ADMIN_PORT}"
+fi
+
 CONTAINER_NAME=ext-auth-proxy
 DOCKERFILE=envoy.Dockerfile
 
-docker stop ${CONTAINER_NAME} || true
-docker rm ${CONTAINER_NAME} || true
-docker rmi -f ${CONTAINER_NAME} || true
 docker build -f ${DOCKERFILE} -t ${CONTAINER_NAME} .
-docker run -d -p "${PORT}":"${PORT}" -p 19000:19000 --name ${CONTAINER_NAME} ${CONTAINER_NAME}
+docker run -d -p "${PORT}":"${PORT}" -p "${ADMIN_PORT}":"${ADMIN_PORT}" --name ${CONTAINER_NAME} ${CONTAINER_NAME}
---
+++ ext-authz-proxy/clean_envoy_docker.sh
@@ -0,0 +1,8 @@
+# Enable exit on non 0
+set -e
+
+CONTAINER_NAME=ext-auth-proxy
+
+docker stop ${CONTAINER_NAME} 2> /dev/null || true
+docker rm ${CONTAINER_NAME} 2> /dev/null || true
+docker rmi -f ${CONTAINER_NAME} 2> /dev/null || true
