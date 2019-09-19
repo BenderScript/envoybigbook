b525ab003fc6c9b02b1afd106900ba7ad5944893
repenno <rapenno@gmail.com>
2019-09-19 00:01:42 -0700

    Fixing infinite redirection in forward proxy example

--- forward-proxy/create_iptables.sh
+++ forward-proxy/create_iptables.sh
@@ -1,6 +1,6 @@
 # Enable exit on non 0
 set -e
 
-sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 80 -j REDIRECT --to-port 4999
-sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8443
+sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 80 -m owner ! --uid-owner 0 -j REDIRECT --to-port 4999
+sudo iptables -t nat -A OUTPUT -p tcp -m tcp --dport 443 -m owner ! --uid-owner 0 -j REDIRECT --to-ports 8443
 
